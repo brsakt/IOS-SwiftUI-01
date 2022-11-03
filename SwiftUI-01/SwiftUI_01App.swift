c1f6c2a2334db9f46cc056f598a50aa04d638785
baris <akturk_baris@hotmail.com>
2022-11-03 12:49:00 +0300

    Initial Commit

---
+++ SwiftUI-01/SwiftUI_01App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_01App.swift
+//  SwiftUI-01
+//
+//  Created by BARIŞ AKTÜRK on 3.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
